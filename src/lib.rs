e10da5dca7b23f4adee69fc8422282c9f8995a4f
tim <tanzhenyennn@gmail.com>
2020-03-14 03:27:44 +0000

    finsih writing file to hdfs

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,41 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+extern crate libc;
 
 #[cfg(test)]
 mod tests {
+
+    use std::ffi::CString;
+    use libc::c_void;
+    use super::*;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+    #[test]
+    fn basic_hdfs(){
+	unsafe{
+		let name_node = CString::new("default").expect("CString::new failed");
+		let fs = hdfsConnect(name_node.as_ptr(), 0);
+
+		let write_path = CString::new("t.txt").expect("CString::new failed");
+		let write_file = hdfsOpenFile(fs, write_path.as_ptr(), (O_WRONLY |O_CREAT) as i32, 0, 0, 0);
+
+		let buffer = String::from("Hello world\n");
+		let buffer_len = buffer.len();
+		let buffer_ptr = CString::new(buffer).expect("CString::new failed");
+		let buffer_ptr = buffer_ptr.as_ptr() as *const c_void;
+
+		let written_bytes = hdfsWrite(fs, write_file, buffer_ptr, buffer_len as i32);
+
+		let result = hdfsFlush(fs, write_file);
+
+		hdfsCloseFile(fs, write_file);
+		
+
+	}
+   }
 }
