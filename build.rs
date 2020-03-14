8f35d11323a18d005f00e9d02be971873e3ade70
tim <tanzhenyennn@gmail.com>
2020-03-14 01:03:02 +0000

    finish bindgen setting

---
+++ build.rs
@@ -0,0 +1,20 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    println!("cargo:rustc-link-lib=hdfs");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+	.clang_arg("-I$HADOOP_HDFS_HOME/include")
+	.clang_arg("-L$HADOOP_HDFS_HOME/lib/native")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ wrapper.h
@@ -0,0 +1,1 @@
+#include "src/hdfs.h"
